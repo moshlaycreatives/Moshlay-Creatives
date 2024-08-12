f705b9ea62cae9c99ea0466260f017bcaed7512d
Ahmadsultan395 <145713175+Ahmadsultan395@users.noreply.github.com>
2024-08-13 04:00:44 +0500

    careers section

--- src/Pages/Careers/components/CareersJob.jsx
+++ src/Pages/Careers/components/CareersJob.jsx
@@ -1,5 +1,9 @@
 import { ThemeContext } from '@emotion/react'
-import { Box, Typography, useTheme } from '@mui/material'
+import { Box, Button, Card, Grid, IconButton, Typography, useTheme } from '@mui/material'
+import { FiShoppingBag } from "react-icons/fi";
+import { TbCurrentLocation } from "react-icons/tb";
+
+
 import React from 'react'
 
 const CareersJob = () => {
@@ -27,6 +31,80 @@ const CareersJob = () => {
             Current Available Positions at Moshlay Creatives
             </Typography>
         </Box>
+
+        {/* ===========================CARDS================================ */}
+
+        <Grid container >
+
+        <Grid item lg={6} md={6} sm={12} xs={12}>
+          <Card sx={{ 
+             padding:'2rem',
+             backgroundColor:'#1b243b', borderRadius:'15px'
+          }}>
+            <Box sx={{
+              display:'flex',
+              justifyContent:'space-between'
+            }}>
+
+              <Box sx={{display:'flex', alignItems:'center'}}>
+                <IconButton>
+                <FiShoppingBag color={'white'}/>
+                </IconButton>
+                <Typography sx={{fontSize:'1rem', color:'white', fontWeight:'400'}}>
+                  Frontend Developer - Full Time
+                </Typography>
+              </Box>
+              <Button variant='contained' sx={{
+                backgroundColor:'#3d4659',
+                fontSize:'0.9rem',
+                fontWeight:400,
+                 color:'white', borderRadius:'20px', padding:'.8rem 1.5rem'
+              }}> Designer</Button>
+            </Box>
+            <br />
+            <Typography sx={{fontSize:'1rem', color:'white', fontWeight:'700', marginLeft:'.7rem'}}>
+                UI | Ux Desinger
+              </Typography>
+              <br />
+
+            <Box sx={{display:'flex'}}>
+            <Box sx={{display:'flex', alignItems:'center'}}>
+            <IconButton>
+            <TbCurrentLocation color={'white'}/>
+                </IconButton>
+                <Typography sx={{fontSize:'1rem', color:'white', fontWeight:'400'}}>
+                  Lahore - Pakistan
+                </Typography>
+            </Box>
+
+            <Box sx={{display:'flex', alignItems:'center'}}> 
+            <IconButton>
+            <TbCurrentLocation color={'white'} />
+                </IconButton>
+                <Typography sx={{fontSize:'1rem', color:'white', fontWeight:'400'}}>
+                  1-3 years
+                </Typography>
+            </Box>
+            </Box>
+            <br /><br />
+
+
+            <Button variant='outlined' sx={{color:'white', border:'1px solid white',
+              fontSize:'1rem', fontFamily:500,
+              padding:'.8rem 2rem'
+            }}>
+              Apply Now
+            </Button>
+
+            
+
+          </Card>
+
+        </Grid>
+
+        </Grid>
+
+
     </Box>
     
     </>
